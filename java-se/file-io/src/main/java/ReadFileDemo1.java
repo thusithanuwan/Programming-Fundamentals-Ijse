2b03a6357661cf02a4d7f524d36e860e61b7befc
thusithanuwan <nuwwan12403482@gmail.com>
2023-02-27 11:32:18 +0530

    Implmenting file read methods

--- java-se/file-io/src/main/java/ReadFileDemo1.java
+++ java-se/file-io/src/main/java/ReadFileDemo1.java
@@ -1,2 +1,40 @@
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+
 public class ReadFileDemo1 {
+
+    public static void main(String[] args) throws IOException {
+
+        File file = new File("/home/thusitha/Desktop/pic.jpg");
+
+        if (!file.exists()) {                              //   Can check existence of a file.
+            System.out.println("Invalid file path");
+            return;
+        }
+
+        FileInputStream fis = new FileInputStream(file);
+        byte[] bytes = new byte[fis.available()];               // file.available() fives the size of the file.
+        fis.read(bytes);
+
+//        int read1 = fis.read();          // If needed, can read byte by byte.
+//        int read2 = fis.read();
+//        int read3 = fis.read();
+
+        fis.close();   // Should close() streams after using.
+
+        System.out.println(Arrays.toString(bytes));
+
+        System.out.println(new String(bytes));
+    }
+
+}
+
+
+
+
 }
--- java-se/file-io/src/main/java/ReadFileDemo2.java
+++ java-se/file-io/src/main/java/ReadFileDemo2.java
@@ -1,2 +1,27 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class ReadFileDemo2 {
+    public static void main(String[] args) throws IOException {
+        File file = new File("/home/thusitha/Desktop/pic.jpg");
+
+        if (!file.exists()) {                              //  We can check existence of
+            System.out.println("Invalid file path");
+            return;
+        }
+
+        FileInputStream fis = new FileInputStream(file);
+
+        byte[] bytes = fis.readAllBytes();           //  We can read all the bytes using readAllBytes() method.
+
+        fis.close();
+
+        System.out.println(Arrays.toString(bytes));
+
+        System.out.println(new String(bytes));     // We can convert byte[] to a string using new String() method.
+
+    }
 }
