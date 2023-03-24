d0943051b35f37dfecb7cd354a7855516ee57c9d
thusithanuwan <nuwwan12403482@gmail.com>
2023-03-24 13:21:58 +0530

    Implemented MyAnnotation

--- lombok/lombok/src/main/java/lk/ijse/dep10/MyAnnotation.java
+++ lombok/lombok/src/main/java/lk/ijse/dep10/MyAnnotation.java
@@ -1,5 +1,17 @@
 package lk.ijse.dep10;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD}) // @Target is used to specify where this annotation can use
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface MyAnnotation {
+
+//    public abstract void    print();
+    public abstract String name();
+    public abstract int getNumber() default 10;
 
 }
